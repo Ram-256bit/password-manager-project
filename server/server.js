c050227e80b0499ddf16121301afac838dc251c4
Ram-256bit <72127084+Ram-256bit@users.noreply.github.com>
2024-11-10 12:46:53 +0530

    1

---
+++ server/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose"
+import cookieParser from "cookie-parser";
+import { authentication, toptCheck } from "./src/middlewares/authentication.js"
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
+
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://localhost:27017/pm4", { useNewUrlParser: true })
+  .then(() => { console.log("DB connected!!") })
+  .catch(() => { console.log("Check DB connection!!") })
+
+import UserAuthRoutes from "./src/routes/User/UserAuthRoutes.js"
+import VaultRoutes from "./src/routes/Vault/VaultRoutes.js"
+
+app.get('/', (req, res) => {
+  res.send("Hello From Server!!")
+})
+
+// ====User Routes====
+app.use('/api/user', UserAuthRoutes)
+
+// ====Vault Routes ====
+app.use('/api', authentication, toptCheck, VaultRoutes)
+
+app.listen(port, () => {
+  console.log(`Runnning on port ${port}`);
+})
